b51226d92d0dc91c0720a0748ddcf2fc94c86d21
aheadley <aheadley@waysaboutstuff.com>
2012-09-05 22:16:45 -0400

    stub some funcs

---
+++ git-hub.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import git
+import pygithub3
+import argparse
+
+def create_parser():
+    pass
+
+def init_github():
+    pass
+
+def init_repo():
+    pass
+
+if __name__ == '__main__':
+    pass
