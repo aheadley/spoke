56467ffb307d457cf6ae8253d9031cd17d3db44b
aheadley <aheadley@waysaboutstuff.com>
2012-09-05 23:30:54 -0400

    add repo creation as test

--- git-hub.py
+++ git-hub.py
@@ -1,17 +1,62 @@
 #!/usr/bin/env python
 
+""" Git plugin for GitHub integration
+"""
+
+import argparse
+import os
+try:
+    import ConfigParser
+except ImportError: #renamed in py3k
+    import configparser as ConfigParser
+
 import git
 import pygithub3
-import argparse
 
 def create_parser():
-    pass
+    parser = argparse.ArgumentParser(description='git-hub - Do stuff with GitHub',
+        prog='git-hub')
+    parser.add_argument('namespace')
+    parser.add_argument('verb')
+    parser.add_argument('options', nargs='*')
+    return parser
 
-def init_github():
-    pass
+def init_github(repo):
+    if repo is None:
+        if os.path.exists(os.path.expanduser('~/.gitconfig')):
+            cfg = git.config.GitConfigParser(os.path.expanduser('~/.gitconfig'))
+        else:
+            raise ValueError('Can\'t find a config file for github login info')
+    else:
+        cfg = repo.config_reader()
+    gh_username = cfg.get_value('hub', 'username')
+    gh_password = cfg.get_value('hub', 'password')
+    repo_name = os.path.basename(repo.working_tree_dir) \
+        if repo is not None else None
+    gh = pygithub3.Github(login=gh_username, user=gh_username,
+        password=gh_password, repo=repo_name)
+    return gh
 
 def init_repo():
-    pass
+    try:
+        repo = git.Repo(os.getcwd())
+    except git.exc.InvalidGitRepositoryError as err:
+        repo = None
+    return repo
+
+def handle_parsed(parser_result, gh, repo):
+    if parser_result.namespace == 'repos':
+        if parser_result.verb == 'create':
+            repo = gh.repos.create(data=dict(
+                name=parser_result.options[0],
+                description=' '.join(parser_result.options[1:])))
+            print repo, vars(repo)
+
+def main():
+    repo = init_repo()
+    gh = init_github(repo)
+    parser = create_parser()
+    handle_parsed(parser.parse_args(), gh, repo)
 
 if __name__ == '__main__':
-    pass
+    main()
